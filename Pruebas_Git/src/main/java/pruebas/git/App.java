8ca3fb06e52dcfbec062a2782218866df3ec1c9a
Martin Lucero <luceromartinandres89630@gmail.com>
2013-11-17 03:50:57 -0300

    Inicio

---
+++ Pruebas_Git/src/main/java/pruebas/git/App.java
@@ -0,0 +1,13 @@
+package pruebas.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
